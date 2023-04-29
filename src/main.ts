b63c3d7b3afacabec5376d86333c6017da0fe789
Fredrik Andersson <fredrik.ks.andersson@gmail.com>
2023-04-29 16:02:03 +0200

    draw first rectangle with pixi.js

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
+import { Application, Graphics } from 'pixi.js';
 
 function main() {
-  console.log('hello world');
+  const container = document.querySelector('#app');
+  if (!container) {
+    console.error('Could not find container for the game!');
+    return;
+  }
+  const app = new Application();
+  document.body.appendChild(app.view as any);
+
+  const graphics = new Graphics();
+  graphics.beginFill(0xFF0000);
+  graphics.drawRect(0, 0, 300, 200);
+  app.stage.addChild(graphics);
 }
 
-main();
+document.addEventListener('DOMContentLoaded', () => {
+  main();
+});
+

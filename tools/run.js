d6276389e885773a02c6a26c28a51bfdf35f987f
Fredrik Andersson <fredrik.ks.andersson@gmail.com>
2023-04-29 15:52:50 +0200

    add initial ts project

---
+++ tools/run.js
@@ -0,0 +1,31 @@
+const {exec} = require('child_process');
+const fs = require('fs');
+
+function watchFile(fileName, signal) {
+  fs.cp(fileName, `dist/${fileName}`, {recursive: true}, () => {});
+  fs.watch(fileName, {signal}, () => {
+    fs.cp(fileName, `dist/${fileName}`, {recursive: true}, () => {});
+  });
+}
+
+function main() {
+  const serverChild = exec('npx http-server dist -c-1');
+  const esbuildChild = exec('npx esbuild src/main.ts --bundle --outdir=dist --watch');
+
+  const abortController = new AbortController();
+
+  watchFile('index.html', abortController.signal);
+  watchFile('styles.css', abortController.signal);
+
+  serverChild.stdout?.on('data', console.log);
+  esbuildChild.stdout?.on('data', console.log);
+
+  process.on('exit', () => {
+    abortController.abort();
+    serverChild.kill('SIGKILL');
+    esbuildChild.kill('SIGKILL');
+  })
+}
+
+
+main();
